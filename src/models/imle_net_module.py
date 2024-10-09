5a9eac56d8bc937dde44bae395f32ec6fe68f167
ntthongk66 <21020408@vnu.edu.vn>
2024-10-09 20:14:38 +0700

    fix loss

--- src/models/imle_net_module.py
+++ src/models/imle_net_module.py
@@ -115,11 +115,11 @@ class IMLENetLitModule(LightningModule):
         """
         x, y = batch
         logits = self.forward(x)
-        y = torch.argmax(y, dim=1)
+        
         loss = self.criterion(logits, y)
         preds = torch.sigmoid(logits)
         preds = torch.argmax(logits, dim=1)
-        
+        y = torch.argmax(y, dim=1)
         return loss, preds, y
 
     def training_step(
